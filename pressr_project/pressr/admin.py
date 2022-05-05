5b1587393b729cb660ffe2e3d909ca78d029c931
Dan Ventura <danice.ventura@gmail.com>
2022-05-05 14:53:56 -0600

    added other models, admin url set-up

--- pressr_project/pressr/admin.py
+++ pressr_project/pressr/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Reading, Comment
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Reading)
+admin.site.register(Comment)
--- pressr_project/pressr/models.py
+++ pressr_project/pressr/models.py
@@ -5,8 +5,8 @@ from datetime import date
 
 
 class User(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, blank=False)
+    last_name = models.CharField(max_length=100, blank=False)
     # https://stackoverflow.com/questions/22212479/display-date-of-birth-in-django-app
     dob = models.DateField()
     photo_url = models.TextField()
@@ -33,3 +33,27 @@ class User(models.Model):
             return today.year - born.year - 1
         else:
             return today.year - born.year
+
+    def __str__(self):
+        return self.name
+
+
+class Reading(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='readings')
+    systolic = models.IntegerField(max_length=3)
+    diastolic = models.IntegerField(max_length=3)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    reading = models.ForeignKey(
+        Reading, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.name
