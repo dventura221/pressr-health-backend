3cacb22b4b3e336bb9d90def155951ba788d8546
Dan Ventura <danice.ventura@gmail.com>
2022-05-09 13:02:27 -0600

    created views

--- pressr_project/pressr/views.py
+++ pressr_project/pressr/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import ProviderSerializer, UserSerializer, ReadingSerializer, CommentSerializer
+from .models import Provider, User, Reading, Comment
 # Create your views here.
+
+
+class ProviderList(generics.ListCreateAPIView):
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
+
+
+class ProviderDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ReadingList(generics.ListCreateAPIView):
+    queryset = Reading.objects.all()
+    serializer_class = ReadingSerializer
+
+
+class ReadingDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Reading.objects.all()
+    serializer_class = ReadingSerializer
+
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
