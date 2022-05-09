018cb2736d5b9c30fd23340dbd33ff4994f8ce67
Dan Ventura <danice.ventura@gmail.com>
2022-05-09 08:46:25 -0600

    finalized new provider class, adjusted str return

--- pressr_project/pressr/admin.py
+++ pressr_project/pressr/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Reading, Comment
+from .models import User, Reading, Comment, Provider
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Reading)
 admin.site.register(Comment)
+admin.site.register(Provider)
--- pressr_project/pressr/models.py
+++ pressr_project/pressr/models.py
@@ -1,19 +1,8 @@
 from django.db import models
+# from django.contrib.contenttypes.models import ContentType
+# from django.contrib.contenttypes import fields
 
 # Create your models here.
-
-
-class User(models.Model):
-    first_name = models.CharField(max_length=100, blank=False)
-    last_name = models.CharField(max_length=100, blank=False)
-    # https://stackoverflow.com/questions/22212479/display-date-of-birth-in-django-app
-    dob = models.DateField(blank=True, null=True)
-    photo_url = models.TextField(null=True)
-    provider = models.ForeignKey(
-        'self', on_delete=models.PROTECT, blank=True, null=True, )
-
-    def __str__(self):
-        return (self.last_name + ", " + self.first_name)
 
 
 class Provider(models.Model):
@@ -32,6 +21,19 @@ class Provider(models.Model):
         return (self.last_name + ", " + self.first_name + ", " + self.provider_type)
 
 
+class User(models.Model):
+    first_name = models.CharField(max_length=101, blank=False)
+    last_name = models.CharField(max_length=100, blank=False)
+    # https://stackoverflow.com/questions/22212479/display-date-of-birth-in-django-app
+    dob = models.DateField(blank=True, null=True)
+    photo_url = models.TextField(null=True)
+    provider = models.ForeignKey(
+        Provider, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return (self.last_name + ", " + self.first_name)
+
+
 class Reading(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='readings')
@@ -39,15 +41,25 @@ class Reading(models.Model):
     diastolic = models.IntegerField()
 
     def __str__(self):
-        return self.name
+        return (str(self.systolic) + " / " + str(self.diastolic))
+
+
+comment_choices = (
+    ('User', 'User'),
+    ('Provider', 'Provider')
+)
 
 
 class Comment(models.Model):
+    author_of_comment = models.CharField(
+        max_length=8, choices=comment_choices, default=None)
+    provider = models.ForeignKey(
+        Provider, on_delete=models.CASCADE, blank=True, null=True)
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User, on_delete=models.CASCADE, blank=True, null=True)
     reading = models.ForeignKey(
         Reading, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField(max_length=1000)
 
     def __str__(self):
-        return self.name
+        return (self.content)
